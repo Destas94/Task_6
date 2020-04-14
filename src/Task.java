6be04282c961be600b5750034c8850cd02ab0bed
Dmitry <dim4n94@yandex.ru>
2020-04-14 23:27:31 +0300

    1234

--- src/Task.java
+++ src/Task.java
@@ -11,7 +11,27 @@ public class Task {
         System.out.print("Введите Z: ");
         int Z = in2.nextInt();
         int C = (X+Y+Z)/3;
-        System.out.print("Среднее арифметическое: "+ C);
+        System.out.println("Среднее арифметическое: "+ C);
+        int A = C/2;
+        int D = C%2;
+//        if(D == 0){
+//            System.out.println("Среднее арифметическое делится без остатка, результат: " + A);
+//        }else{
+//            System.out.println("Среднее арифметическое не делится без остатка.");
+//        }
+        if(D == 0){
+            System.out.println("Результат деления без остатка: " + A);
+        }else{
+            int B = C-D;
+            int E = B/2;
+            System.out.println("Результат деления без остатка: " + E);
+        }
+        if(C>3){
+            System.out.println("Программа выполнена корректно.");
+        }else{
+            System.out.println("Программа выполнена не корректно.");
+        }
+
 
     }
 }
